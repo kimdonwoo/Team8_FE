72dce7db35e91888684bb0392e214942e731d4a4
kimdonwoo <ehstjstod@gmail.com>
2023-11-10 20:50:16 +0900

    axios.ts 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -3,28 +3,28 @@ import axios, { InternalAxiosRequestConfig } from 'axios';
 import { getCookie } from 'typescript-cookie';
 
 export const instance = axios.create({
-    baseURL: `/api`,
+  baseURL: `/api`,
 });
 
 instance.interceptors.request.use((config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-    const accessToken = getCookie('access-token');
-    if (accessToken) {
-        const newConfig = config;
-        newConfig.headers.Authorization = accessToken;
+  const accessToken = getCookie('access-token');
+  if (accessToken) {
+    const newConfig = config;
+    newConfig.headers.Authorization = accessToken;
 
-        return newConfig;
-    }
-    return config;
+    return newConfig;
+  }
+  return config;
 });
 
 instance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        if (error.response.data.error.message === '링크 실패') {
-            return Promise.resolve(error.response);
-        }
-        return Promise.reject(error);
-    },
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.data.error.message === '링크 실패') {
+      return Promise.resolve(error.response);
+    }
+    return Promise.reject(error);
+  },
 );
